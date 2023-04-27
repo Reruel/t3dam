8bf4b1d84b00b924641ee9ae70dcb7a5113bf318
Reruel <reyesrubioelena@gmail.com>
2023-04-27 11:58:19 +0200

    ,

---
+++ saludos/EjTaTeTi.js
@@ -0,0 +1,16 @@
+'use strict';
+// Código de Reyes Rubio Elena
+// 1º DAM, Lenguaje de Marcas
+// Fecha: 27 de abril de 2023
+
+// variable para los círculos
+var colorfuente = "white";
+
+// defino un array con colores, tipo constante
+const colores = [];
+colores[0] = "green";
+colores[1] = "brown";
+colores[2] = "blue";
+colores[3] = "beige";
+colores[4] = "magenta";
+
